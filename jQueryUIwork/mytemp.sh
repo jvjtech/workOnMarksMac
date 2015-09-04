377fe4393269c02c9251b80c9248e06e33c73a54
JohnJ <jvj@golden.net>
2015-09-04 08:07:59 -0400

    restructure directories to match jqueryui.com/demos

---
+++ jQueryUIwork/mytemp.sh
@@ -0,0 +1,39 @@
+# mkdir Interactions
+# mkdir Interactions\Draggable
+# mkdir Interactions\Droppable
+# mkdir Interactions\Resizable
+# mkdir Interactions\Selectable
+# mkdir Interactions\Sortable
+# mkdir Widgets
+# mkdir Widgets\Accordion
+# mkdir Widgets\Autocomplete
+# mkdir Widgets\Button
+# mkdir Widgets\Datepicker
+# mkdir Widgets\Dialog
+# mkdir Widgets\Menu
+# mkdir Widgets\Progressbar
+# mkdir Widgets\Selectmenu
+# mkdir Widgets\Slider
+# mkdir Widgets\Spinner
+# mkdir Widgets\Tabs
+# mkdir Widgets\Tooltip
+# mkdir Effects
+# mkdir Effects\Add Class
+# mkdir Effects\ColorAnimation
+# mkdir Effects\Easing
+# mkdir Effects\Effect
+# mkdir Effects\Hide
+# mkdir Effects\RemoveClass
+# mkdir Effects\Show
+# mkdir Effects\SwitchClass
+# mkdir Effects\Toggle
+# mkdir Effects\ToggleClass
+# mkdir Utilities
+# mkdir Utilities\Position
+# mkdir Utilities\WidgetFactory
+# git mv dialogAmime.htm dialogAnime.htm
+git mv dialogAnime.htm Widgets\Dialog\dialogAnime.htm
+git mv dialogBasicModal.htm Widgets\Dialog\dialogBasicModal.htm
+git mv dialogModalForm.htm Widgets\Dialog\dialogModalForm.htm
+git mv dialogModalMessage.htm Widgets\Dialog\dialogModalMessage.htm
+git mv anotherdialog.htm Widgets\Dialog\anotherdialog.htm
